34052d0be236ca01632dc72e1513b14e013cfc5f
htlin222 <ppoiu87@gmail.com>
2022-10-17 19:01:16 +0800

    add __main__

--- text-to-speech.py
+++ text-to-speech.py
@@ -12,17 +12,19 @@ SSML_SETTINGS = '<voice name="zh-TW-YunJheNeural"><prosody rate="15%" pitch="+5%
 SSML_START = '<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">'
 SSML_END = '</prosody></voice></speak>'
 
-with open('translated.txt') as f:
-    lines = f.readlines()
-    # text = lines[0]
+def text_to_speech(input_file):
+    with open(input_file) as f:
+        lines = f.readlines()
+        # text = lines[0]
+    i = 1
+    for text in lines:
+        ssml = SSML_START + SSML_SETTINGS + text + SSML_END
+        result = synthesizer.speak_ssml_async(ssml).get()
+        stream = AudioDataStream(result)
+        filename = "output/" + str(i) + "_語音_" + text[0:5] + '.wav'
+        stream.save_to_wav_file(filename)
+        i = i + 1
 
-# TODO: add for loop for each line
-i = 1
-for text in lines:
-    # TODO: regex to delete the markdown and empty lines
-    ssml = SSML_START + SSML_SETTINGS + text + SSML_END
-    result = synthesizer.speak_ssml_async(ssml).get()
-    stream = AudioDataStream(result)
-    filename = "output/" + str(i) + "_語音_" + text[0:5] + '.wav'
-    stream.save_to_wav_file(filename)
-    i = i + 1
+if __name__=='__main__':
+    text_to_speech('translated.txt')
+    print('語音完成')
--- translate-the-original.py
+++ translate-the-original.py
@@ -1,19 +1,23 @@
 from googletrans import Translator
 import os
 
-with open('original.txt') as f:
-    lines = f.readlines()
+def translate_to_zh():
+    with open('original.txt') as f:
+        lines = f.readlines()
 
-translated_list = []
+    translated_list = []
 
-for text in lines:
-    translator = Translator()
-    translated =translator.translate(text, dest='zh-tw')
-    translated_list.append(translated.text)
-# pc.copy(text1)
+    for text in lines:
+        translator = Translator()
+        translated =translator.translate(text, dest='zh-tw')
+        translated_list.append(translated.text)
+    # pc.copy(text1)
 
-with open(r'translated.txt', 'w') as fp:
-    for item in translated_list:
-        # write each item on a new line
-        fp.write("%s\n" % item)
+    with open(r'translated.txt', 'w') as fp:
+        for item in translated_list:
+            # write each item on a new line
+            fp.write("%s\n" % item)
+
+if __name__=='__main__':
+    translate_to_zh()
     print('翻譯完成')
