11f2a9790edee03702e0af1689989841d7624e8e
htlin222 <hsieh.ting.lin@gmail.com>
2022-10-20 17:19:14 +0800

    edit: batch.py exclude README.md

--- batch.py
+++ batch.py
@@ -4,14 +4,13 @@ import sys
 from pathlib import Path
 import main as main
 
-for file in os.listdir("/mydir"):
-    if file.endswith(".txt"):
-        print(os.path.join("/mydir", file))
 
 if __name__=='__main__':
     cwd = os.getcwd()
     for file in os.listdir(cwd):
         if file.endswith(".md") or file.endswith(".txt"):
-            folder = re.sub(r'\.[A-Za-z]*','', filename)
-            main.project_init(file, folder)
-            print("✨Have generated the", folder, "project")
+            if not file.endswith("README.md"):
+                filename = str(file)
+                folder = re.sub(r'\.[A-Za-z]*','', filename)
+                main.project_init(file, folder)
+                print("✨Have generated the", folder, "project")
