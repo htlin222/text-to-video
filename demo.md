ca9e3a586cdd3e9b439144256cd79a2294b45c10
htlin222 <hsieh.ting.lin@gmail.com>
2022-10-21 12:17:02 +0800

    add calcuated duration of wav

---
+++ demo.md
@@ -0,0 +1,5 @@
+clinical practice. gastroesophageal reflux disease.
+A 55-year-old woman presents with symptoms of heartburn and an 8-year history of a sour, bitter taste in her mouth. Her symptoms have increased in frequency and severity over the past year and now occur daily, typically after meals and at nighttime. She has no dysphagia, odynophagia, weight loss, anorexia, or upper gastrointestinal bleeding. 
+Her medical history includes hypertension (treated with lisinopril) and obesity; the body-mass index (BMI, the weight in kilograms divided by the square of the height in meters) is 31. She smoked a half pack of cigarettes daily for 20 years but stopped 15 years ago. She eats late dinners that usually include wine. 
+She has tried antacids and famotidine (at a dose of 20 mg twice daily) for a few months with minimal effect on the symptoms. 
+How would you manage this case?
--- export_video.py
+++ export_video.py
@@ -4,21 +4,25 @@ import sys
 from pathlib import Path
 import subprocess
 import re
+import soundfile as sf
 
 # subprocess.run(ffmpeg -i ep1.png -i ep1.wav ep1.mp4)
 # output_file_path = folder + '/' + 'slide.pptx'
 def img_and_audio(input_image,input_audio,output_video):
     '''
-    combine an image to wav file to mp4
+    combine an image to wav file to mp4 according to wav duration, which calculated by frames devided by samplerate
     '''
-    subprocess.run(f"ffmpeg -y -i {input_image} -i {input_audio} -pix_fmt yuv420p {output_video}", shell=True)
-
+    audio = sf.SoundFile(input_audio)
+    duration = audio.frames / audio.samplerate
+    settings = "-c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -t " + str(duration)
+    subprocess.run(f"ffmpeg -y -loop 1 -i {input_image} -i {input_audio} {settings} {output_video}", shell=True)
 
 def batch_img_and_audio(folder):
     '''
     load files from audio_list.txt
     '''
     i = 1
+    # read the audio_list from txt files and import them into a list
     the_audio_list = folder + "/" + "audio_list.txt"
     with open(the_audio_list) as the_list:
         lines = the_list.readlines()
@@ -26,15 +30,17 @@ def batch_img_and_audio(folder):
     for item in lines:
         item = re.sub('\n','',item) # remove the line break
         input_audio = item
-        # change to jpg if you want
+        # if your export image as .jpg, them change the .png to it
         input_image = folder + "/slide/" + "投影片" + str(i) + ".png"
         output_video = folder + "/" + "clip" + str(i) + ".mp4"
         if os.path.exists(input_audio):
             img_and_audio(input_image, input_audio, output_video)
+        # create the clip list
         filename = "file " + folder + "/" + "clip" + str(i) + ".mp4"
         filename = "file " + "clip" + str(i) + ".mp4"
         clips_list.append(filename)
         i = i + 1
+    # write clips_list into concat.txt, which will be used later for combine into one video
     clips_txt = folder + '/' + 'concat.txt'
     with open(clips_txt, 'w+') as clips_txt:
         for item in clips_list:
@@ -45,7 +51,7 @@ def batch_img_and_audio(folder):
 def export_final(output_folder):
     working_dir = output_folder
     final = str(output_folder) + "_final.mp4"
-    subprocess.run(f"ffmpeg -y -f concat -i concat.txt -c copy {final}", cwd=working_dir, shell=True)
+    subprocess.run(f"ffmpeg -y -f concat -safe 0 -i concat.txt -c copy {final}", cwd=working_dir, shell=True)
 
 def check_folder(output_folder):
     current_directory = os.getcwd()
