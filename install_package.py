ebd1ba7ed9cb4c8ceb1daa49ef196d1c69c9ddd2
HsiehTing Lin <hsieh.ting.lin@gmail.com>
2022-10-24 00:18:38 +0800

    add autodetect install package fuction

--- install_package.py
+++ install_package.py
@@ -10,18 +10,54 @@ open_manual = input("Do you want to open online manual? (y/n)")
 if open_manual == "y":
     webbrowser.open('https://github.com/htlin222/text-to-video',new=2)
 
-pip_install = input("Do you want to install all pip? (y/n)")
-if pip_install == "y":
-    subprocess.run(f"pip install googletrans==4.0.0-rc1 azure-cognitiveservices-speech pypandoc playsound pdf2image", shell=True)
+print("Start to import python package, if not installed, will install it")
 
-have_homebrew = input("Do you have installed homebrew? :(y/n)")
+def check_import_or_install():
+    '''
+    install all package needed
+    '''
+    try:
+        from googletrans import Translator
+    except ImportError as e:
+        print(e, 'strat to install')
+        subprocess.run(f"pip install googletrans==4.0.0-rc1", shell=True)
+    try:
+        import azure.cognitiveservices.speech
+    except ImportError as e:
+        print(e, 'strat to install')
+        subprocess.run(f"pip install azure-cognitiveservices-speech", shell=True)
+    try:
+        import azure.cognitiveservices.speech
+    except ImportError as e:
+        print(e, 'strat to install')
+        subprocess.run(f"pip install azure-cognitiveservices-speech", shell=True)
+    try:
+        import pypandoc
+    except ImportError as e:
+        print(e, 'strat to install')
+        subprocess.run(f"pip install pypandoc", shell=True)
+    try:
+        import playsound
+    except ImportError as e:
+        print(e, 'strat to install')
+        subprocess.run(f"pip install playsound", shell=True)
+    try:
+        import pdf2image
+    except ImportError as e:
+        print(e, 'strat to install')
+        subprocess.run(f"pip install pdf2image", shell=True)
+        subprocess.run(f"pip3 install PyObjC", shell=True)
+    print("\nAll package installed 🎉\n")
+check_import_or_install()
+
+have_homebrew = input("Have you ever installed homebrew? :(y/n)")
 if have_homebrew == "y":
     subprocess.run(r"brew install ffmpeg",shell=True)
     subprocess.run(r"brew install pandoc",shell=True)
     subprocess.run(r"brew install --cask libreoffice",shell=True)
     # install Libreoffice
 else:
-    answer = input("Open the webbrowser to install [ffmpeg, pandoc, and libreoffice] manually? : (y/n)")
+    answer = input("Open the 👁️ webbrowser to install [1️⃣ ffmpeg, 2️⃣ pandoc, and 3️⃣ libreoffice] manually? : (y/n)")
     if answer == "y":
         webbrowser.open('https://github.com/jgm/pandoc/releases/tag/2.19.2',new=2)
         webbrowser.open('https://www.libreoffice.org/download/download-libreoffice/',new=2)
